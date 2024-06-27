d727002795241bbb2a724cd74095f39ab3a3b31c
aurellio <aurellio.rajendra14@gmail.com>
2024-06-27 18:52:29 +0700

    update projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -80,7 +80,7 @@ const Projects = () => {
                     Yu-Gi-Oh!
                   </h1>
                   <div className="text-white text-[14px] bg-[#ED4C51] rounded-full py-[8px] px-[14px] flex items-center">
-                    <p>Personal Project</p>
+                    <p>Kampus Merdeka</p>
                   </div>
                 </div>
                 <p className="text-[#8B8689] md:text-[18px] text-[14px]">
